82eb93bb143fd74f39ef16eca9d6e7b9db682da7
Cotel <miguelcoletomunoz@hotmail.com>
2017-10-25 12:25:53 +0200

    Fixing imports and gradle

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -1,12 +1,5 @@
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.firefox.FirefoxDriver
-import org.openqa.selenium.remote.DesiredCapabilities
-import org.openqa.selenium.remote.RemoteWebDriver
-
-/**
- * Created by ConradoMateu on 25/10/2017.
- */
 
 
 fun main(args: Array<String>) {
