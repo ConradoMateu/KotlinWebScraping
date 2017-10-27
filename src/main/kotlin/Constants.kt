4d19ca093e28cb6becbaf0760d3855c33a462b7c
Conrado Mateu Gisbert <conradomateugisbert@gmail.com>
2017-10-27 10:52:56 +0200

    Added StoresRepos

---
+++ src/main/kotlin/Constants.kt
@@ -0,0 +1,19 @@
+/**
+ * Created by ConradoMateu on 26/10/2017.
+ */
+
+object CONSTANTS{
+
+    object CHROME{
+        val TYPE = "webdriver.chrome.driver"
+        val PATH = System.getProperty("user.dir")+"/Chrome/chromedriver"
+    }
+
+    object AMAZON{
+        val URL = "https://amazon.es"
+    }
+
+    object CORTEINGLES{
+        val URL = "https://elcorteingles.es"
+    }
+}
---
+++ src/main/kotlin/datasource/Stores/StoreRepository.kt
@@ -0,0 +1,18 @@
+package datasource.Stores
+
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+
+/**
+ * Created by ConradoMateu on 26/10/2017.
+ */
+
+interface StoreRepository{
+    var driver: WebDriver
+    fun browse()
+    fun findElement(by: By): WebElement
+    fun findElements(by:By): List<WebElement>
+    fun configureWebDriver(type: String = CONSTANTS.CHROME.PATH, path: String = CONSTANTS.CHROME.TYPE)
+    fun search(element: WebElement,text: String)
+}
