d244931ac3ec0642d14ad0db20cd5fa26c4116ce
Conrado Mateu Gisbert <conradomateugisbert@gmail.com>
2017-11-11 03:46:26 +0100

    Sorted Brands

--- src/main/kotlin/MainActivity.kt
+++ src/main/kotlin/MainActivity.kt
@@ -49,7 +49,7 @@ class MainActivity : View() {
                 hgrow = Priority.ALWAYS
 
                 label("Seleccione las marcas")
-                brandsListView = listview(presenter.brands) {
+                brandsListView = listview(presenter.brands.sorted()) {
                     selectionModel.selectionMode = SelectionMode.MULTIPLE
                 }
             }
