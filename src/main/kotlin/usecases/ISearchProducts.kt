0dd39c3bd5944d38888ad49203d33e5ce86ef717
Cotel <miguelcoletomunoz@hotmail.com>
2017-11-12 19:53:16 +0100

    Improving buzzwords

--- src/main/kotlin/usecases/ISearchProducts.kt
+++ src/main/kotlin/usecases/ISearchProducts.kt
@@ -17,7 +17,7 @@ abstract class ISearchProducts {
                 "de", "para", "capsulas", "espresso", "automatica", "filtro", "molinillo", "multibebida",
                 "electrica", "expresso", "vivy", "grind", "goteo", "brew", "superautomatica", "n",
                 "senseo", "intense", "coffee", "maker", "blanco", "blanca", "con", "jarra",
-                "dolce", "gusto", "f", "nescafe", "italiana", "hidro-presion", "russell", "hobbs"
+                "dolce", "gusto", "f", "nescafe", "italiana", "hidro-presion", "russell", "hobbs", "super"
         ).union(brandsDAO.getAll().map(String::toLowerCase)).toList()
     }
 
